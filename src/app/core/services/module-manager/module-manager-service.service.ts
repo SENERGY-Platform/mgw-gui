a2609e7e7209962fbbcfe6afd5df359d48681905
Hannes Hansen <mail@hannesh.de>
2023-11-27 15:13:56 +0100

    use correct http method for deleting deployment

--- src/app/core/services/module-manager/module-manager-service.service.ts
+++ src/app/core/services/module-manager/module-manager-service.service.ts
@@ -118,7 +118,7 @@ export class ModuleManagerService {
     if(forceConfirmed) {
       queryParams = queryParams.set("force", "true")
     }
-    return <Observable<any>>this.http.patch(url, undefined, queryParams, 'text');
+    return <Observable<any>>this.http.delete(url, undefined, queryParams, 'text');
    }
 
   deleteDeployments(deploymentIDs: string[], forceConfirmed: boolean): Observable<any> {
